7d8398946cf7be244eda5fde60db4da69d32f532
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-17 20:37:03 +0100

    aktualizace

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 //Catering
+/*
 const justFood = (pocetLidi) => {
-
+	return `catering od Just Food pro ${pocetLidi} lidí za ${(pocetLidi * 100
+	)} Kč`
 }
 
 const yourMama = (pocetLidi) => {
-    
-}
+    return `
+    catering od Your Mama pro ${pocetLidi} lidí za ${(pocetLidi * 250
+        )} Kč`
+    }
 
 const flavourHaven = (pocetLidi) => {
-    
+    return `
+    catering od Flavour Haven pro ${pocetLidi} za ${(pocetLidi * 800
+        )} Kč`
+    }
+ 
+const createEvent = (nazevUdalosti, pocetLidi, cateringSpolecnosti) => {
+    return ` Událost ${nazevUdalosti} s catering pro ${pocetLidi} lidí za ${cateringSpolecnosti(pocetLidi)}
+    Kč`
 }
+
+document.body.innerHTML += createEvent(
+    'Inaugurace prezidenta',
+    150,
+    flavourHaven
+)
+*/
+
+//Kostka
+const hod= (side) => {
+    onclick
+}
+let side = document.querySelector('.dice')
+if onkeydown = side.classList.toggle('dice')
